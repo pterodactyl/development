9d5545069f1ca72295657e57656a558821ace41d
DaneEveritt <dane@daneeveritt.com>
2025-12-20 16:55:53 -0800

    Final cleanup

--- docker/mysql/00-setup.sql
+++ docker/mysql/00-setup.sql
@@ -1,3 +1,6 @@
-create database if not exists testing;
-grant all on testing.* TO 'pterodactyl'@'%';
+create database if not exists pterodactyl_test;
+
+grant all on pterodactyl_test.* TO 'pterodactyl'@'%';
+grant all on `pterodactyl\_test\_test_%`.* TO 'pterodactyl'@'%';
+
 flush privileges;
